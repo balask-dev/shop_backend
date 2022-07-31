e7e98c4d5239268e67e789f025d221c7ebc87c78
Bala <92292670+balask-dev@users.noreply.github.com>
2022-07-31 17:27:40 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -5,18 +5,22 @@
  import  jwt from "jsonwebtoken";
  import bcrypt from "bcrypt";
 
- router.post("/register", async (req, res) => {
+router.post("/register", async (req, res) => {
   try {
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
-    const newUser = new User({...req.body,password: hash,});
-    await newUser.save();
-    res.status(200).send("Account Created");
-  } catch (err) {
-       console.log(err) 
- }
-})
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashed,
+    });
 
+    const user = await newUser.save();
+    res.send(user);
+  } catch (err) {
+    res.send(err);
+  }
+});
  
 router.post("/login", async (req, res) => {
   try {
